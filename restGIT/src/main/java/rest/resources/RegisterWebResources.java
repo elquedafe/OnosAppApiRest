2b8d5bd8e5e32e330a4ce2e9f59283d62ea9be8e
Alvaro Lus <alvaro.perkin@gmail.com>
2019-06-30 19:40:01 +0200

    new register resource

--- restGIT/src/main/java/rest/resources/RegisterWebResources.java
+++ restGIT/src/main/java/rest/resources/RegisterWebResources.java
@@ -11,6 +11,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.ws.rs.Consumes;
@@ -25,17 +26,18 @@ import com.google.gson.Gson;
 
 import rest.gsonobjects.onosside.OnosResponse;
 import rest.gsonobjects.userside.UserCredentials;
+import tools.DatabaseTools;
 import tools.LogTools;
 
 @Path("/rest/register")
 public class RegisterWebResources {
-private Gson gson;
-	
+	private Gson gson;
+
 	public RegisterWebResources() {
 		LogTools.info("RegisterWebResources", "***ONOS SIMPLE REST API (OSRA) Service***");
 		gson = new Gson();
 	}
-	
+
 	/**
 	 * Create a meter for the given switch
 	 * @param switchId Switch ID where meter is installed
@@ -47,73 +49,16 @@ private Gson gson;
 	@Produces (MediaType.APPLICATION_JSON)	
 	public Response register(String jsonIn) {
 		UserCredentials credentials = gson.fromJson(jsonIn, UserCredentials.class);
-//		boolean registered = false;
-//		String db = "users.cnf";
-//		try {
-//
-//            File f = new File(db);
-//
-//            BufferedReader b = new BufferedReader(new FileReader(f));
-//
-//            String readLine = "";
-//
-//            System.out.println("Reading file using Buffered Reader");
-//            String[] user;
-//            while ((readLine = b.readLine()) != null) {
-//            	user = readLine.split(":");
-//            	if(user[0].equals(credentials.getUser()) && user[1].equals(credentials.getPassword())) {
-//            		registered = true;
-//            		break;
-//            	}
-//                System.out.println(readLine);
-//            }
-//            b.close();
-//            if(!registered) {
-//            	try {
-//            	    Files.write(Paths.get("users.cnf"), (credentials.getUser()+":"+credentials.getPassword()+System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
-//            	}catch (IOException e) {
-//            	    //exception handling left as an exercise for the reader
-//            	}
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-		
-		String propertiesFilename = "users.properties";
-	
-		try {
-			InputStream input = new FileInputStream(propertiesFilename);
-			
-            Properties prop = new Properties();
-            prop.load(input);
-            
-            System.out.println("PROP " +prop);
-            
-            String user = prop.getProperty(credentials.getUser());
-            LogTools.info("register", "user retrieved "+user);
-            input.close();
-            
-            //If user already exists
-            if(user != null && !user.isEmpty()) {       
-            	return Response.ok(new OnosResponse("User already registered",400)).build();
-            }
-            //If new user then store
-            else {
-            	prop.setProperty(credentials.getUser(), credentials.getPassword());
-            	//CREATE NEW USER
-            }
-            
-			OutputStream output = new FileOutputStream(propertiesFilename);
-            prop.store(output, null);
-            output.close();
-            
-            System.out.println(prop);
 
-        } catch (IOException io) {
-            io.printStackTrace();
-            return Response.ok(new OnosResponse("Error registering",400)).build();
-        }
+		try {
+			DatabaseTools.register(credentials.getUser(), credentials.getPassword(), false);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			Response.ok(new OnosResponse("Error registering",400)).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.ok(new OnosResponse("Error registering",400)).build();
+		}
 		return Response.ok(new OnosResponse("User registered successfuly",200)).build();
 	}
 
